18b6ab573fdd980d6d069c359f63ac4dc392e5fc
Spencer Weaver <sweaver@eventideinvestments.com>
2023-01-05 14:37:48 -0500

    finished wait_for_db && migrate

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom Django management commands.
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -21,14 +21,16 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
-    def test_wait_for_db_delay(self, patched_check):
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting error"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
+

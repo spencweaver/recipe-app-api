e2531405a31f2111a2c3c08810735706c7343dde
Spencer Weaver <spencweaver@gmail.com>
2023-10-27 16:37:55 -0400

    created method to assign a recipe to a user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -13,6 +13,7 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     """Create and return a new user"""
     return get_user_model().objects.create_user(**params)
@@ -37,7 +38,6 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
-
 
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
@@ -65,7 +65,6 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
-
 
     def test_create_token_for_user(self):
         """Test generates token for valid credentials."""
